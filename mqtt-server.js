fcf0fec8bfcbdd8d30b9554e9e93149f8ba25d08
Roy van de Water <dev+roy@integrumtech.com>
2014-10-01 13:54:08 -0700

    Fix mqtt payload format

--- mqtt-server.js
+++ mqtt-server.js
@@ -251,7 +251,7 @@ server.on('published', function(packet, client) {
       sendMessage(client.skynetDevice, JSON.parse(packet.payload.toString()));
     }
     else if('tb' === packet.topic){
-      sendMessage(client.skynetDevice, packet.payload.toString(), 'tb');
+      sendMessage(client.skynetDevice, {payload: packet.payload.toString(), devices: ['*']}, 'tb');
     }
     else if('directText' === packet.topic){
       sendMessage(client.skynetDevice, JSON.parse(packet.payload.toString()), 'tb');
@@ -295,6 +295,6 @@ server.on('published', function(packet, client) {
       });
     }
   }catch(ex){
-    console.log('error publishing');
+    console.log('error publishing', ex.stack);
   }
 });
